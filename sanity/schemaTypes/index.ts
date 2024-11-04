d7e32bdc830925a8f1452f0ed9acaeab10d7a1f0
devnadish <devnadish@gmail.com>
2024-11-04 12:45:56 +0300

    fix schema add department

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,12 +1,21 @@
-import { type SchemaTypeDefinition } from 'sanity'
+import { type SchemaTypeDefinition } from "sanity";
 
-import {blockContentType} from './blockContentType'
-import {categoryType} from './categoryType'
-import {postType} from './postType'
-import {authorType} from './authorType'
-import { jobs } from './jobs'
-import { jobsType } from './jobsType'
+import { blockContentType } from "./blockContentType";
+import { categoryType } from "./categoryType";
+import { postType } from "./postType";
+import { authorType } from "./authorType";
+import { jobs } from "./jobs";
+import { jobsType } from "./jobsType";
+import { departmentType } from "./departmentType";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [blockContentType, categoryType, postType, authorType,jobs,jobsType],
-}
+  types: [
+    blockContentType,
+    categoryType,
+    postType,
+    authorType,
+    jobs,
+    jobsType,
+    departmentType,
+  ],
+};
